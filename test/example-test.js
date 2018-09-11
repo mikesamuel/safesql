f6e5abf252cd184a644673c0c15c1df555aa69f5
Mike Samuel <mikesamuel@gmail.com>
2018-09-11 18:01:43 -0400

    fleshed out test

--- test/example-test.js
+++ test/example-test.js
@@ -15,21 +15,60 @@
  * limitations under the License.
  */
 
-/* eslint "id-length": off */
+/* eslint "id-length": 0, "id-blacklist": 0 */
 
 'use strict';
 
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
+
 const { sql } = require('../index');
+const { SqlId } = require('../id');
 
 describe('example code', () => {
   describe('README.md', () => {
     // These mirror example code in ../README.md so if you modify this,
     // be sure to reflect changes there.
-    it('stub', () => {
-      // TODO
-      expect(sql !== null).to.equal(true);
+
+    it('SELECT various', () => {
+      const ids = [ SqlId.escape('x'), SqlId.escape('y') ];
+      const table = 'table';
+      const id = 'foo\'"bar';
+
+      const query = sql`SELECT (${ ids }) FROM \`${ table }\` WHERE id=${ id }`;
+
+      expect(query.content).to.equal(
+        'SELECT (`x`, `y`) FROM `table` WHERE id=\'foo\\\'\\"bar\'');
+    });
+    it('UPDATE obj', () => {
+      const column = 'users';
+      const userId = 1;
+      const data = {
+        email: 'foobar@example.com',
+        modified: sql`NOW()`,
+      };
+      const query = sql`UPDATE \`${ column }\` SET ${ data } WHERE \`id\` = ${ userId }`;
+
+      expect(query.content).to.equal(
+        'UPDATE `users` SET `email` = \'foobar@example.com\', `modified` = NOW() WHERE `id` = 1');
+    });
+    it('chains', () => {
+      const data = { a: 1 };
+      const whereClause = sql`WHERE ${ data }`;
+      expect(sql`SELECT * FROM TABLE ${ whereClause }`.content).to.equal(
+        'SELECT * FROM TABLE WHERE `a` = 1');
+    });
+    it('no excess quotes', () => {
+      expect(sql`SELECT '${ 'foo' }' `.content).to.equal('SELECT \'foo\' ');
+      expect(sql`SELECT ${ 'foo' } `.content).to.equal('SELECT \'foo\' ');
+    });
+    it('backtick delimited', () => {
+      expect(sql`SELECT \`${ 'id' }\` FROM \`TABLE\``.content).to.equal(
+        'SELECT `id` FROM `TABLE`');
+    });
+    it('raw escapes', () => {
+      expect(sql`SELECT "\n"`.content).to.equal(
+        String.raw`SELECT "\n"`);
     });
   });
 });
